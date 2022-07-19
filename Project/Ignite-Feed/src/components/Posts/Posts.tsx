a42a1a42cd3b0778ac8e5fdac0fd1af6b3947900
arthur-art <arthurteixeira.guts@gmail.com>
2022-07-19 15:38:20 -0300

    Estilizando Posts

--- Project/Ignite-Feed/src/components/Posts/Posts.tsx
+++ Project/Ignite-Feed/src/components/Posts/Posts.tsx
@@ -1,3 +1,6 @@
+import { imgGithubProfile } from "../../assets/url-photos";
+import styles from "./Posts.module.css"
+
 interface PostProps{
     post:string;
 }
@@ -5,6 +8,28 @@ interface PostProps{
 export const Posts = ({post}:PostProps) =>{
 
     return (
-        <h1>{post}</h1>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img src={imgGithubProfile} alt="Github Profile" />
+                    <div className={styles.authorInfo}>
+                        <strong>Arthur Teixeira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Texto</p>
+                <p><a href="">arthur.web/github</a></p>
+                <p>
+                    <a href="">#rocketseat</a>{' '}
+                    <a href="">#github</a>{' '}
+                    <a href="">#react</a>{' '}
+                </p>
+            </div>
+        </article>
     )
 }
