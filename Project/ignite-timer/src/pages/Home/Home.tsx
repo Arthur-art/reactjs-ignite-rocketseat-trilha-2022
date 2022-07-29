5147963c6f53fa4431acf6bb1905f2cf17aa3c8f
arthur-art <arthurteixeira.guts@gmail.com>
2022-07-29 16:35:01 -0300

    Estilizando page Home

--- Project/ignite-timer/src/pages/Home/Home.tsx
+++ Project/ignite-timer/src/pages/Home/Home.tsx
@@ -1,7 +1,37 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+} from './Home.styles'
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <FormContainer action="">
+        <div>
+          <label htmlFor="task">Vou Trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+          <span>minutos</span>
+        </div>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </FormContainer>
+    </HomeContainer>
   )
 }
