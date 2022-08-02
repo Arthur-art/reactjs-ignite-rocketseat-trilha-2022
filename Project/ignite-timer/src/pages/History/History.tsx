91fef3292e2a08ac885e260052cb94a18b7ba8a4
arthur-art <arthurteixeira.guts@gmail.com>
2022-08-02 13:46:38 -0300

    Desenvolvendo a page History, estilizando table

--- Project/ignite-timer/src/pages/History/History.tsx
+++ Project/ignite-timer/src/pages/History/History.tsx
@@ -1,7 +1,47 @@
+import { HistoryContainer, HistoryList } from './History.styles'
+
 export const History = () => {
   return (
-    <div>
+    <HistoryContainer>
       <h1>History</h1>
-    </div>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Estudar React</td>
+              <td>30 minutos</td>
+              <td>Há 3 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Estudar Nest</td>
+              <td>30 minutos</td>
+              <td>Há 3 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Estudar React Native</td>
+              <td>30 minutos</td>
+              <td>Há 3 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Estudar Vue</td>
+              <td>30 minutos</td>
+              <td>Há 3 dias</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
