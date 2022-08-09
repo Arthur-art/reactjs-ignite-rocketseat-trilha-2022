71c980f255e02e0498347b1ed253c7a041b87730
arthur-art <arthurteixeira.guts@gmail.com>
2022-08-08 21:44:00 -0300

    Adicionando timer no title da page

--- Project/ignite-timer/src/pages/Home/Home.tsx
+++ Project/ignite-timer/src/pages/Home/Home.tsx
@@ -54,12 +54,17 @@ export const Home = () => {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -73,6 +78,7 @@ export const Home = () => {
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
     reset()
   }
 
@@ -84,6 +90,12 @@ export const Home = () => {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
