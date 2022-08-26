7b5d1bec2136bc8bd2d40a61dedd4ac9bd1f2cd2
arthur-art <arthurteixeira.guts@gmail.com>
2022-08-26 16:16:02 -0300

    Salvando estado no localStorage

--- Project/ignite-timer/src/pages/Home/components/Countdown/Countdown.tsx
+++ Project/ignite-timer/src/pages/Home/components/Countdown/Countdown.tsx
@@ -34,7 +34,7 @@ export const CountDown = () => {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDifference >= totalSeconds) {
