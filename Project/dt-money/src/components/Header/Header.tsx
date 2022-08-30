57968178319fa6881b0c41126c22d2f285ae78d0
arthur-art <arthurteixeira.guts@gmail.com>
2022-08-29 23:31:34 -0300

    Desenvolvendo component Header

---
+++ Project/dt-money/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
