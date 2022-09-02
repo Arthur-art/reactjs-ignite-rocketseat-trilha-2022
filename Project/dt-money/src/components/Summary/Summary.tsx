fba3744b51bb830be713c8701d93a3eb8f822da5
arthur-teixeira <rhitmo.arthur.teixeira@seedz.ag>
2022-09-02 15:17:50 -0300

    Desenvolvendo component Summary

--- Project/dt-money/src/components/Summary/Summary.tsx
+++ Project/dt-money/src/components/Summary/Summary.tsx
@@ -1,35 +1,35 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
-import { SummaryContainer } from './styles'
+import { SummaryCard, SummaryContainer } from './styles'
 
 export const Summary = () => {
   return (
     <SummaryContainer>
-      <div>
+      <SummaryCard>
         <header>
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard>
         <header>
           <span>Saidas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
 
-      <div>
+      <SummaryCard variant="green">
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
         <strong>R$ 17.400,00</strong>
-      </div>
+      </SummaryCard>
     </SummaryContainer>
   )
 }
--- Project/dt-money/src/components/Summary/styles.ts
+++ Project/dt-money/src/components/Summary/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const SummaryContainer = styled.section`
   width: 100%;
@@ -11,4 +11,33 @@ export const SummaryContainer = styled.section`
   gap: 2rem;
 
   margin-top: -5rem;
+`
+interface SummaryCardProps {
+  variant?: 'green'
+}
+
+export const SummaryCard = styled.div<SummaryCardProps>`
+  background: ${(props) => props.theme['gray-600']};
+  border-radius: 6px;
+  padding: 2rem;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    color: ${(props) => props.theme['gray-300']};
+  }
+
+  strong {
+    display: block;
+    margin-top: 1rem;
+    font-size: 2rem;
+  }
+
+  ${(props) =>
+    props.variant === 'green' &&
+    css`
+      background: ${props.theme['green-700']};
+    `}
 `
