723a9d0c5a065461068a26c7ee91a03d80f2a733
arthur-teixeira <rhitmo.arthur.teixeira@seedz.ag>
2022-09-05 12:49:10 -0300

    Trabalhando com lib Radix para lidar com components primitivos com mais acessibilidade, Iniciando o desenvolvimento do modal da aplicacao

--- Project/dt-money/src/components/Header/Header.tsx
+++ Project/dt-money/src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
 
 export const Header = () => {
@@ -6,7 +7,19 @@ export const Header = () => {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
