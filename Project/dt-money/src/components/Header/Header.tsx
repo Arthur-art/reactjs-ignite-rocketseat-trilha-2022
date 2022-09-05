a5281ad6a52171606abf3e4d10c52ed517bd9e52
arthur-teixeira <rhitmo.arthur.teixeira@seedz.ag>
2022-09-05 13:26:28 -0300

    Finalizando style do modal de nova transacao

--- Project/dt-money/src/components/Header/Header.tsx
+++ Project/dt-money/src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 import logoImg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal/NewTransactionModal'
 
 export const Header = () => {
   return (
@@ -12,13 +13,7 @@ export const Header = () => {
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
