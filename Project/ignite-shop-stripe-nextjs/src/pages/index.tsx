ef09a580ac01b891bbd000d92ca2d359d5dfbfed
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-22 15:04:25 -0300

    Estilizando page Home

--- Project/ignite-shop-stripe-nextjs/src/pages/index.tsx
+++ Project/ignite-shop-stripe-nextjs/src/pages/index.tsx
@@ -1,23 +1,41 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  background: '$gray100',
-  border: 'none',
-  borderRadius: '5px',
-  width: '5rem',
-  height: '2rem',
-  fontSize: '1.2rem',
-  cursor: 'pointer',
-  '&:hover': {
-    opacity: '0.8',
-  },
-  transition: '0.3s',
-})
+import Image from 'next/future/image'
+import shirt1 from '../assets/1.png'
+import shirt2 from '../assets/2.png'
+import shirt3 from '../assets/3.png'
+import shirt4 from '../assets/4.png'
+import { HomeContainer, Product } from '../styles/pages/home'
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image width={520} height={480} src={shirt1} alt="shirt" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image width={520} height={480} src={shirt2} alt="shirt" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image width={520} height={480} src={shirt3} alt="shirt" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image width={520} height={480} src={shirt4} alt="shirt" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span> R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
