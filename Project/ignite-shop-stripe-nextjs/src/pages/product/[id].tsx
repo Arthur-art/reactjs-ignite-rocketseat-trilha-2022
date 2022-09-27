17aa74124127fc690aea667c5412f9ebff827f6c
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-27 14:36:01 -0300

    Desenvolvendo SSG com parametro dinamico

--- Project/ignite-shop-stripe-nextjs/src/pages/product/[id].tsx
+++ Project/ignite-shop-stripe-nextjs/src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Stripe from 'stripe'
@@ -43,6 +43,13 @@ export default function Product({ product }: ProductProps) {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: 'prod_MTspllE3GZ7Rot' } }],
+    fallback: false,
+  }
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
