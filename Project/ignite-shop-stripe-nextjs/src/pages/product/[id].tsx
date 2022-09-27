2cf829faad1d903700928803d1dfe03c47d7e460
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-27 13:51:45 -0300

    Desenvolvendo a page de produtos

--- Project/ignite-shop-stripe-nextjs/src/pages/product/[id].tsx
+++ Project/ignite-shop-stripe-nextjs/src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+          obcaecati provident soluta natus quas optio iste illo deserunt,
+          voluptas nobis ab eos ex in. Excepturi quo dicta qui perferendis nemo.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
