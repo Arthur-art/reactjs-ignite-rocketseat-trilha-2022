437d5810b7b2fc7a8e51067522e43973b1a38184
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-27 14:19:34 -0300

    Carregando dados do produto

--- Project/ignite-shop-stripe-nextjs/src/pages/product/[id].tsx
+++ Project/ignite-shop-stripe-nextjs/src/pages/product/[id].tsx
@@ -1,16 +1,35 @@
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '../../styles/pages/product'
 
-export default function Product() {
+type ProductsType = {
+  id: string
+  name: string
+  imageUrl: string
+  url: string
+  price: number
+  description: string
+}
+
+interface ProductProps {
+  product: ProductsType
+}
+
+export default function Product({ product }: ProductProps) {
   const { query } = useRouter()
 
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image width={520} height={480} src={product.imageUrl} alt="shirt" />
+      </ImageContainer>
       <ProductDetails>
         <h1>Camiseta X</h1>
         <span>R$ 79,90</span>
@@ -24,4 +43,33 @@ export default function Product() {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const procuctId = params.id
+
+  const product = await stripe.products.retrieve(procuctId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        url: product.url,
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1hour
+  }
 }
