31c5b8b7496f89e072eeee80147ca78d7a22bb03
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-27 13:16:27 -0300

    Ajustando layout

--- Project/ignite-shop-stripe-nextjs/src/styles/pages/app.ts
+++ Project/ignite-shop-stripe-nextjs/src/styles/pages/app.ts
@@ -11,6 +11,6 @@ export const Container = styled('div', {
 export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
-  maxWidth: 1180,
+  maxWidth: 1500,
   margin: '0 auto',
 })
--- Project/ignite-shop-stripe-nextjs/src/styles/pages/home.ts
+++ Project/ignite-shop-stripe-nextjs/src/styles/pages/home.ts
@@ -3,9 +3,8 @@ import { styled } from '..'
 export const HomeContainer = styled('main', {
   display: 'flex',
   width: '100%',
-  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
-  minHeight: 656,
+  maxWidth: 1500,
 })
 
 export const Product = styled('a', {
