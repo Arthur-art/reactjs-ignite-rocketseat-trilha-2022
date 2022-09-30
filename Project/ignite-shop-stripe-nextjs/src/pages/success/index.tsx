5b0580fcaa0d9537eb319ff2d5bd363cf2ce5e76
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-30 15:20:44 -0300

    Finalizando page de success, consumindo dados da compra efetuada

--- Project/ignite-shop-stripe-nextjs/src/pages/success/index.tsx
+++ Project/ignite-shop-stripe-nextjs/src/pages/success/index.tsx
@@ -1,21 +1,64 @@
+import { GetServerSideProps } from 'next'
+import Image from 'next/image'
 import Link from 'next/link'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import {
   SuccessContainer,
   SuccessImageContainer,
 } from '../../styles/pages/success'
 
-export default function Success() {
+interface SuccessProps {
+  custummerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+export default function Success({ custummerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
-      <SuccessImageContainer></SuccessImageContainer>
+      <SuccessImageContainer>
+        <Image
+          width={120}
+          height={110}
+          src={product.imageUrl}
+          alt="productImage"
+        />
+      </SuccessImageContainer>
 
       <p>
-        A sua camiseta <strong>Beyond the limits</strong> já está à caminho da
-        sua casa!
+        Sucesso <strong> {custummerName}, </strong>o seu produto{' '}
+        <strong>{product.name}</strong> já está à caminho da sua casa!
       </p>
 
       <Link href="/">Voltar à página inicial.</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+  params,
+}) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const custummerName = session.customer_details.name
+  const product = session.line_items.data[0].price.product as Stripe.Product
+
+  return {
+    props: {
+      custummerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
 }
