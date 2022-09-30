a5fce6f141d1f6eb148778f882d38642ec0636c1
arthur-art <arthurteixeira.guts@gmail.com>
2022-09-30 15:32:35 -0300

    Trabalhando com redirect no SSR

--- Project/ignite-shop-stripe-nextjs/src/pages/success/index.tsx
+++ Project/ignite-shop-stripe-nextjs/src/pages/success/index.tsx
@@ -39,10 +39,16 @@ export default function Success({ custummerName, product }: SuccessProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({
-  query,
-  params,
-}) => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
   const sessionId = String(query.session_id)
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
